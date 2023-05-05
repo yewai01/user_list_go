cb69da15613ffcfb6c7c5fd24c761604d6e01558
Ye Wai <scm.yewai@gmail.com>
2023-05-05 14:31:18 +0630

    ok

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -2,6 +2,9 @@ package main
 
 import (
 	"github.com/labstack/echo/v4"
+	// "github.com/yewai01/user_list_go/app/handlers"
+	// "github.com/yewai01/user_list_go/app/repositories"
+	// "github.com/yewai01/user_list_go/app/services"
 )
 
 func main() {
